c74d13b5b000906bec00bfd46f35a06206d30141
Linzhongren <6215714@qq.com>
2017-07-07 00:11:32 +0800

    合约管理UI初步实现分页显示（分页查询还未实现）

--- jpress/jpress-model/src/main/java/yjt/model/query/ContractQuery.java
+++ jpress/jpress-model/src/main/java/yjt/model/query/ContractQuery.java
@@ -18,20 +18,26 @@ public class ContractQuery extends JBaseQuery{
 		return QUERY;
 	}
 	
-	public List<Contract> findList(int page, int pageSize, String status){
+	public List<Contract> findList(int page, int pageSize, Contract.Status cs){
 		StringBuilder sqlBuilder = new StringBuilder("Select * From contract as c ");
 		LinkedList<Object> params = new LinkedList<Object>();
 		
 		boolean needWhere = true;
-		needWhere = appendIfNotEmpty(sqlBuilder, "c.status", status, params, needWhere);
+		if(cs != null)
+			needWhere = appendIfNotEmpty(sqlBuilder, "c.status", String.valueOf(cs.getIndex()), params, needWhere);
 		
 		sqlBuilder.append(" Order By c.id Desc Limit ?, ?");
-		params.add(page -1);
+		params.add((page -1)*pageSize);
 		params.add(pageSize);
 		if(params.isEmpty()){
 			return DAO.find(sqlBuilder.toString());
 		}else{
 			return DAO.find(sqlBuilder.toString(), params.toArray());
 		}
+	}
+	
+	public long findCountByStatus(Contract.Status cs) {
+		if(cs == Contract.Status.ALL) return DAO.doFindCount();
+		else return DAO.doFindCount("status = ?", cs.getIndex());
 	}
 }
--- jpress/jpress-web-admin/src/main/java/io/jpress/admin/controller/yjt/_ContractController.java
+++ jpress/jpress-web-admin/src/main/java/io/jpress/admin/controller/yjt/_ContractController.java
@@ -20,11 +20,18 @@ public class _ContractController extends JBaseCRUDController<Contract>{
 	
 	@PermAnnotation("contract-list")
 	public void index(){
-		List<Contract> list = ContractQuery.me().findList(1, 10, null);
+		long total_records = ContractQuery.me().findCountByStatus(Contract.Status.ALL);
+		setAttr("total_records", total_records);
+		int page_index = (getPara("page") != null) ? Integer.parseInt(getPara("page")) : 1;
+		setAttr("page_index", page_index);
+		int page_size = (getPara("pagesize") != null) ? Integer.parseInt(getPara("pagesize")) : 10;
+		setAttr("page_size", page_size);
+		int total_pages = (int)Math.ceil(((double)total_records)/page_size);
+		setAttr("total_pages", total_pages);
+		List<Contract> list = ContractQuery.me().findList(page_index, page_size, null);
 		setAttr("include", "_index_include.html");
 		setAttr("list", list);
 		render("index.html");
-		//this.renderText(JSONObject.toJSONString(list));
 	}
 	
 	@PermAnnotation("contract-stat")
